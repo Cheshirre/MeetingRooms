7b8096226e945a00640766c2206b2a9e65d78b06
Alena Melnik <melnik_e@goldapple.ru>
2019-10-25 12:46:44 +0500

    try hide white line

--- www/js/index.js
+++ www/js/index.js
@@ -46,6 +46,11 @@ var app = {
 		
 		var ref = window.open('http://ekb-app_srv.ga.loc:8090/meetingrooms', '_blank', 'location=no,toolbar=no');
 		ref.addEventListener('loadstart', function() { alert(event.url); });
+		
+		if (window.StatusBar) {
+		  StatusBar.overlaysWebView(false);
+		  StatusBar.overlaysWebView(true);
+		}
 
         console.log('Received Event: ' + id);
     }
